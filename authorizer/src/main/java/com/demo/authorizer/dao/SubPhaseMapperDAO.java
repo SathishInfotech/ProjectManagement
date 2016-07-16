2da652c8134bbf86de14528ba518937268f7df77
umar1990 <ismi1990@gmail.com>
2016-07-16 16:42:23 +0530

    commit issue in eclipse.. commitint directly

---
+++ authorizer/src/main/java/com/demo/authorizer/dao/SubPhaseMapperDAO.java
@@ -0,0 +1,7 @@
+package com.demo.authorizer.dao;
+
+import com.demo.authorizer.entity.PhaseSubPhaseMapper;
+
+public interface SubPhaseMapperDAO extends GenericDAO<PhaseSubPhaseMapper, Integer> {
+	
+}
